38058027de62237a87e23983244ee0580482de9a
iki1uc <entertrainer@web.de>
2026-08-12 11:42:59 +0200

    Update NET.js

--- NET.js
+++ NET.js
@@ -3,3 +3,11 @@ SUS = {
   can: ["zeigen", "durchlassen", "vektorisieren"],
   cannot: ["erzwingen", "binden", "verändern"]
 }
+// NET.js
+import { SUS } from "./SUS.js";
+
+NET.modules.push(SUS);
+
+NET.route = function(vec) {
+  return SUS.run(vec);
+};
